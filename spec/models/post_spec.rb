54f6c9857478e0fde1d2eec1b862f74c2c5152cf
Jen Strong <jenniferastrong@gmail.com>
2018-08-05 18:40:18 +0100

    Add unit test for Post model

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,4 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
+  it "is valid with a capation" do
+    expect(Post.new(caption: 'caption')).to be_valid
+  end
+
+  it "ist not valid without a caption" do
+    expect(Post.new).to_not be_valid
+  end
 end
